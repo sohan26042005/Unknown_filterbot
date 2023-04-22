b3d4bb23e77b24850d312e386dbb46ea3dc5ded6
sohan26042005 <sohanmandal2005@gmail.com>
2023-04-22 10:43:20 +0530

    some_run

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/sohan26042005/PREMIUMFILTERBOT.git /PREMIUMFILTERBOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /PREMIUMFILTERBOT
 fi
-cd /PiroAutoFilterBot
+cd /PREMIUMFILTERBOT
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting PREMIUMFILTERBOT...."
 python3 bot.py
